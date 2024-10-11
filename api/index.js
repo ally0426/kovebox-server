92fdd78da0dfa6100ce82a268a34f2637d788442
Youngmi Lee <abinding@gmail.com>
2024-10-11 06:18:19 -0500

    cors origin as array []

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 // Configure CORS
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "https://kovebox-client.vercel.app",
+    origin: ["https://kovebox-client.vercel.app", process.env.CLIENT_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true, // Enable this if your API uses cookies
   })
