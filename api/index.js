4b6251ca4ddc83836b2a3eba3014cedbf151bc90
Youngmi Lee <abinding@gmail.com>
2024-10-11 06:04:01 -0500

    added cors handling

--- api/index.js
+++ api/index.js
@@ -15,6 +15,15 @@ const twilio = require("twilio");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Configure CORS
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "https://kovebox-client.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true, // Enable this if your API uses cookies
+  })
+);
+
 // Initialize Twilio client
 const client = twilio(
   process.env.TWILIO_ACCOUNT_SID,
@@ -23,7 +32,7 @@ const client = twilio(
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 app.use(helmet()); // Adding security headers
 
 // API Routes
@@ -35,6 +44,11 @@ app.use("/api/booking", bookingRoutes);
 app.use("/api/payment", paymentRoutes);
 app.use((req, res) => {
   res.status(404).json({ message: "Route not found with 404 ERROR" });
+});
+
+// Test route
+app.get("api", (req, res) => {
+  res.json({ message: "Welcome to the Kovebox API!" });
 });
 
 // Sample route to send an SMS
@@ -72,8 +86,9 @@ app.use((req, res, next) => {
 
 // Centralized Error Handling Middleware
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ message: "An internal error occurred" });
+  console.error("Server ERROR: ", err);
+  // console.error(err.stack);
+  res.status(500).json({ message: "Oh~~~~An internal error occurred" });
 });
 
 // Start the server
